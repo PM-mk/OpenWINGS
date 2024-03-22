db48a6ad26180d44ce4ef3d29714ee8244d5f549
PM-mk <misiar1997@o2.pl>
2024-03-22 23:29:21 +0100

    extend about window

--- include/AboutFrame.hpp
+++ include/AboutFrame.hpp
@@ -3,6 +3,9 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif // WX_PRECOMP
+#include <wx/hyperlink.h>
+#include <wx/statline.h>
+#include <tinyxml2.h>
 
 namespace ow{
     class AboutFrame : public wxDialog{
--- src/AboutFrame.cpp
+++ src/AboutFrame.cpp
@@ -1,12 +1,10 @@
 #include "AboutFrame.hpp"
-#include <wx/hyperlink.h>
 using namespace ow;
 
 AboutFrame::AboutFrame(wxWindow* parent) :
-    wxDialog(parent, wxID_ABOUT, "About", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE)
-{
+    wxDialog(parent, wxID_ABOUT, "About", wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE){
+    // main credits
     wxBoxSizer* pSizerMain = new wxBoxSizer(wxVERTICAL);
-
     wxString aboutOw = "";
     aboutOw += "OpenWINGS Version 0.0.1\n";
     aboutOw += "Open source GUI tool for WINGS and ALMODES system analysis and modelling.\n\n";
@@ -14,26 +12,39 @@ AboutFrame::AboutFrame(wxWindow* parent) :
     wxStaticText* pAboutText = new wxStaticText(this, wxID_ANY, wxString::FromUTF8(aboutOw),
         wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER_HORIZONTAL);
     pAboutText->Wrap(-1);
-
     wxHyperlinkCtrl* pLinkGithub = new wxHyperlinkCtrl(this, wxID_ANY, "github.com/PM-mk/OpenWINGS",
-        wxT("https://github.com/PM-mk/OpenWINGS"), wxDefaultPosition, wxDefaultSize, wxVERTICAL);
+        wxT("https://github.com/PM-mk/OpenWINGS"), wxDefaultPosition, wxDefaultSize);
+	wxStaticLine* pStaticLine = new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL);
+    pSizerMain->Add(pAboutText, 2, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_TOP, 5);
+	// TODO: add program logo
+    pSizerMain->Add(pLinkGithub, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+	pSizerMain->Add(pStaticLine, 0, wxALL|wxEXPAND, 5);
 
-    wxBoxSizer* pSizerSub = new wxBoxSizer(wxHORIZONTAL);
-	pSizerMain->Add(pAboutText, 4, wxALIGN_CENTER_HORIZONTAL|wxALIGN_TOP|wxALL, 5);
-	pSizerMain->Add(pLinkGithub, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
-
-    wxString aboutWx = wxString::Format(wxT("Made with %s"),wxVERSION_STRING);
-    wxStaticText* pWxText = new wxStaticText( this, wxID_ANY, aboutWx, wxDefaultPosition, wxDefaultSize, 0 );
+    // wxWidgets credits
+    wxBoxSizer* pSizerWxCredit = new wxBoxSizer(wxVERTICAL);
+    wxString aboutWx = wxString::Format(wxT("GUI made with %s"),wxVERSION_STRING);
+    wxStaticText* pWxText = new wxStaticText( this, wxID_ANY, aboutWx, wxDefaultPosition, wxDefaultSize);
     pWxText->Wrap(-1);
-
     wxHyperlinkCtrl* pLinkWx = new wxHyperlinkCtrl(this, wxID_ANY, "wxwidgets.org",
-        wxT("https://www.wxwidgets.org"), wxDefaultPosition, wxDefaultSize, wxVERTICAL);
+        wxT("https://www.wxwidgets.org"), wxDefaultPosition, wxDefaultSize);
+    pSizerWxCredit->Add(pWxText, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+	pSizerWxCredit->Add(pLinkWx, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
 
-    pSizerSub->Add(pWxText, 1, wxALL, 5);
-	pSizerSub->Add(pLinkWx, 0, wxALL, 5 );
-	pSizerMain->Add(pSizerSub, 1, wxEXPAND, 5);
-    // TODO: add TinyXML-2 credits
+    // TinyXML-2 credits
+    wxBoxSizer* pSizerXmlCredit = new wxBoxSizer(wxVERTICAL);
+    wxString aboutXml = wxString::Format(wxT("XML files handled with TinyXML-2 v.%i.%i.%i"),
+        TINYXML2_MAJOR_VERSION, TINYXML2_MINOR_VERSION, TINYXML2_PATCH_VERSION);
+    wxStaticText* pXmlText = new wxStaticText( this, wxID_ANY, aboutXml, wxDefaultPosition, wxDefaultSize);
+    pXmlText->Wrap(-1);
+    wxHyperlinkCtrl* pLinkXml = new wxHyperlinkCtrl(this, wxID_ANY, "github.com/leethomason/tinyxml2",
+        wxT("https://github.com/leethomason/tinyxml2"), wxDefaultPosition, wxDefaultSize);
+    pSizerXmlCredit->Add(pXmlText, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+	pSizerXmlCredit->Add(pLinkXml, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
 
+    // finish frame
+    pAboutText->SetFocus(); // set focus on wxStaticText control to prevent focus frame from showing
+	pSizerMain->Add(pSizerWxCredit, 1, wxEXPAND, 5);
+	pSizerMain->Add(pSizerXmlCredit, 1, wxEXPAND, 5);
 	this->SetSizer(pSizerMain);
 	this->Layout();
     this->Fit();
